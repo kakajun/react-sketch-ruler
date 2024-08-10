241f6194e3cf48aaa1503c6233a94e51dc0877eb
kakajun <kakajun@users.noreply.github.com>
2024-08-10 03:40:55 +0000

    deploy: 4a048b19170026020e6bd38df6b03287f088b4ac

--- src/index.tsx
+++
@@ -1,3 +0,0 @@
-import SketchRule  from './sketch-ruler/index'
-export type { SketchRulerProps } from './index-types';
-export default SketchRule
--- types/global.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare type Recordable<T = any> = Record<string, T>
-
-declare type Nullable<T = any> = T | null
-
-declare type Fn<P extends any[] = any[], R = any> = (...args: P) => R
